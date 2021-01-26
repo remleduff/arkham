e4382a08c03791f7624e785006b6b1990be83acb
Joel 'Aaron' Cohen <aaron.cohen@titanonezero.com>
2021-01-26 15:31:45 -0500

    Use constants instead of strings, extract sim to run_sim

--- tokens.py
+++ tokens.py
@@ -4,8 +4,20 @@ from pprint import pprint
 from random import sample
 from tabulate import tabulate
 
+ELDERSIGN = 'eldersign'
+SKULL = 'skull'
+CULTIST = 'cultist'
+TABLET = 'tablet'
+HEART = 'heart'
+
+TENTACLE = 'tentacle'
+
+BLESSING = 'blessing'
+CURSE = 'curse'
+
+
 # One 'cultist removed for flashback'
-# 'cultist',
+# CULTIST,
 bag = [
         0,
         -1,
@@ -19,118 +31,40 @@ bag = [
         -5,
         -6,
         -8,
-        'skull',
-        'skull',
-        'cultist',
-        'tablet',
-        'tablet',
-        'heart',
-        'heart',
-        'tentacle',
-        'eldersign',
+        SKULL,
+        SKULL,
+        CULTIST,
+        TABLET,
+        TABLET,
+        HEART,
+        HEART,
+        TENTACLE,
+        ELDERSIGN,
         ]
 
 spooky = {
-        'blessing': 2,
-        'curse': -2,
-        'eldersign': 1,
-        'skull': -2,
-        'cultist': -2,
-        'tablet': -2,
-        'heart': -3,
+        BLESSING: 2,
+        CURSE: -2,
+        ELDERSIGN: 1,
+        SKULL: -2,
+        CULTIST: -2,
+        TABLET: -2,
+        HEART: -3,
         }
 
-blurses = {'blessing', 'curse'}
-
-#c = combinations([spooky.get(token, token) for token in bag if token != 'tentacle'], 3)
-combos = list(combinations(bag, 3))
-
-total = 0
-n = len(combos)
+blurses = {BLESSING, CURSE}
 
 def is_blursed(token):
     if isinstance(token, list):
-        return 'blessing' in token or 'curse' in token
-    return token == 'blessing' or token == 'curse'
-
-def crystal_pendulum():
-    vals = {}
-    for combo in combos:
-        if 'tentacle' in combo:
-            value = sum(spooky.get(token, token) for token in combo if token != 'tentacle')
-            vals[value] = vals.get(value, 0) + 1
-            total += 1
-        else:
-            for value in set(spooky.get(token, token) for token in combo):
-                vals[value] = vals.get(value, 0) + 1
-                total += 1
-    return vals
-
-
-def resolve_blurse_outcomes(combo, resolved=[]):
-    c = list(combo)
-    t = list(bag)
-    for token in resolved:
-        t.remove(token)
-    for token in combo:
-        t.remove(token)
-        if not is_blursed(token):
-            c.remove(token)
-            resolved.append(token)
-
-    if not c or not is_blursed(c):
-        return [c + resolved]
-
-    print(f'{c}')
-
-    r = []
-    if 'blessing' in c:
-        c.remove('blessing')
-        r += [resolve_blurse_outcomes(c + [token], resolved + ['blessing']) for token in t]
-
-    if 'curse' in c:
-        c.remove('curse')
-        r += [resolve_blurse_outcomes(c + [token], resolved + ['curse']) for token in t]
-
-    return r
-
-
-def skill_test_odds(target=-2):
-    vals = {}
-    for c in combos:
-        combo = list(c)
-
-        cancelled = False
-        outcomes = []
-
-        modifiers = set(spooky.get(token, token) for token in combo if not is_blursed(token) and not token == 'tentacle')
-
-        if 'tentacle' in combo:
-            combo.remove('tentacle')
-            outcomes.append(combo)
-        elif max(modifiers) > target:
-            outcomes.append([max(modifiers)])
-        elif is_blursed(combo):
-            if 'blessing' in combo:
-                combo.remove('blessing')
-                outcomes.extend(resolve_blurse_outcomes(['blessing'], combo))
-            else:
-                combo.remove('curse')
-                outcomes.extend(resolve_blurse_outcomes(['curse'], combo))
-
-
-        #for outcome in outcomes:
-        #    value = sum(spooky.get(token, token) for token in outcome)
-        #    vals[value] = vals.get(value, 0) + 1
-    return vals
-
+        return BLESSING in token or CURSE in token
+    return token == BLESSING or token == CURSE
 
 def blursed(tokens):
     if isinstance(tokens, list):
-        if 'blessing' in tokens:
-            return 'blessing'
-        if 'curse' in tokens:
-            return 'curse'
+        if BLESSING in tokens:
+            return BLESSING
+        if CURSE in tokens:
+            return CURSE
     return None
 
 
@@ -141,12 +75,19 @@ def pull_tokens(bag, n):
 def modifiers(tokens, ignore={}):
     return [spooky.get(token, token) for token in tokens if not token in ignore]
 
+
+def is_success(tokens, difficulty):
+    if TENTACLE in tokens:
+        return False
+    return sum(modifiers(tokens)) > difficulty
+
+
 def jacky_target_difficulty_strat(combo, difficulty=-2, bag=bag):
     outcome = list(combo)
 
     blurse = blursed(combo)
-    if 'tentacle' in combo:
-        outcome.remove('tentacle')
+    if TENTACLE in combo:
+        outcome.remove(TENTACLE)
     elif (m := modifiers(combo, ignore=blurses)) and max(m) > difficulty:
         outcome = [max(m)]
     elif blurse:
@@ -164,40 +105,63 @@ def jacky_target_difficulty_strat(combo, difficulty=-2, bag=bag):
     return outcome
 
 
-def is_success(tokens, difficulty):
-    if 'tentacle' in tokens:
-        return False
-    return sum(modifiers(tokens)) > difficulty
+def crystal_pendulum():
+    vals = {}
+    for combo in combos:
+        if TENTACLE in combo:
+            value = sum(spooky.get(token, token) for token in combo if token != TENTACLE)
+            vals[value] = vals.get(value, 0) + 1
+            total += 1
+        else:
+            for value in set(spooky.get(token, token) for token in combo):
+                vals[value] = vals.get(value, 0) + 1
+                total += 1
+    return vals
 
 
-def monte(bag=bag, difficulty=-2, trials=1000):
+
+def monte(strat, bag=bag, trials=1000):
     result = []
-    successes = 0
     for i in range(trials):
         tokens = pull_tokens(bag, 3)
-        outcome = jacky_target_difficulty_strat(tokens, difficulty, bag)
+        outcome = strat(tokens, bag)
         result.append(outcome)
-        #if i % 100:
-        #    print(f'{tokens} - {difficulty} {curses} {blessings} - {outcome} {is_success(outcome, difficulty)}')
-        if is_success(outcome, difficulty):
-            successes += 1
-    return successes
+    return result
 
 
-trials = 10000
-results = []
-for difficulty in range(-2, 5):
-    for curses in range(0, 10):
-        row = [f'{difficulty}({curses})']
-        for blessings in range(11):
-            successes = monte(bag + ['blessing'] * blessings + ['curse'] * curses, -difficulty, trials)
-            row.append(successes / trials)
-        results.append(row)
+def count_successes(outcomes, difficulty, trials):
+    return sum(is_success(outcome, difficulty) for outcome in outcomes) / trials
 
 
-results.sort(key=lambda x: x[1])
+def best_pendulum_guess(outcomes, difficulty, trials):
+    sail_by = [abs(sum(modifiers(outcome)) - difficulty) for outcome in outcomes if TENTACLE not in outcome]
+    return max(set(sail_by), key=sail_by.count)
+
+
+def run_sim(eval_fn, trials=10000):
+    trials = 10000
+    results = []
+    for difficulty in range(-2, 5):
+        strat = lambda tokens, bag: jacky_target_difficulty_strat(tokens, -difficulty, bag)
+        for curses in range(11):
+            row = [f'{difficulty}({curses})']
+            for blessings in range(11):
+                outcomes = monte(strat, bag + [BLESSING] * blessings + [CURSE] * curses, trials)
+                result = eval_fn(outcomes, difficulty, trials)
+                row.append(result)
+            results.append(row)
+    return results
+
+
+#results = run_sim(count_successes)
+#
+#results.sort(key=lambda x: x[1])
+#print(tabulate(results, headers=['Difficulty(curses)', '0 blessings', *range(1,11)]))
+
+
+print('Most frequent success-by or fail-by value for Pendulum:')
+results = run_sim(best_pendulum_guess)
 print(tabulate(results, headers=['Difficulty(curses)', '0 blessings', *range(1,11)]))
-
 
 
 #vals = skill_test()
@@ -207,5 +171,5 @@ print(tabulate(results, headers=['Difficulty(curses)', '0 blessings', *range(1,1
 #pprint(vals)
 #pprint(probs)
 
-#x = ['blessing', -1, -2, 'curse']
+#x = [BLESSING, -1, -2, CURSE]
 #b = resolve_blurse_outcomes(x)
